b46d489f1c6873aeba5841da7ea399a52b1a5648
manohar628 <venkatamanohar62@gmail.com>
2025-07-15 12:00:36 +0530

    Add files via upload
    basic

---
+++ crt.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int age=15;
+        char alpha='e';
+        long mi=13523576;
+        float hi=2.6f;
+        double bye=3975298729D;
+         System.out.println(age);
+          System.out.println(alpha);
+           System.out.println(mi);
+            System.out.println(hi);
+             System.out.println(bye);
+             
+             String name1="un";
+             String name2="happy";
+             String name3=name1+name2;
+             System.out.println(name3);
+             
+             String name="manohar and  my";
+             System.out.println(name.charAt(4));
+              System.out.println(name.length());
+                          System.out.println(name.substring(7,11));
+                          
+                          String my="manohar";
+                          
+                          System.out.println(my.replace("man" , "dra"));
